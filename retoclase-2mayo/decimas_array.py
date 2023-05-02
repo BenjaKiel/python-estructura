aa6ae5048f200b382dcfa720845d01248f81b947
bentrolllmax <benjamin.kiel@gmail.com>
2023-05-02 18:58:30 -0400

    subiento reto con decimas

---
+++ retoclase-2mayo/decimas_array.py
@@ -0,0 +1,19 @@
+import random
+
+
+arreglo = random.randint(10, 30)
+
+
+array_final = [random.randint(0, 10) for x in range(arreglo)]
+
+
+array_final.sort()
+
+print("Ordenado de forma ascendiente")
+print(array_final)
+
+print("Ordenado de forma aleatoria")
+
+random.shuffle(array_final)
+
+print(array_final)
